3c6d5f7c0d70ed1b40d5adf333873815d65f11b6
Default User <laurence.a.maddox@gmail.com>
2017-09-10 19:01:27 -0500

    adding warnings

--- src/tscpaq-test.c
+++ src/tscpaq-test.c
@@ -8,7 +8,7 @@
 
 int main(void) {
    int arr[] = {101, 202, 303, 404, 505};
-   int *tmp;
+   int const *tmp;
 
    tscpaq_t q;
    tscpaq_alloc_queue (&q, (size_t) N);
@@ -20,7 +20,7 @@ int main(void) {
    /*tscpaq_enqueue (&q, arr + 3);tscpaq_dumpq(&q, 5);
    tscpaq_enqueue (&q, arr + 4);tscpaq_dumpq(&q, 6);*/
    /*memset (arr, 0, sizeof (arr));*/
-   tscpaq_dequeue (&q, (void**)&tmp); printf ("%i\n", *tmp); tscpaq_dumpq(&q, 7);
+   tscpaq_dequeue (&q, (void const *restrict *)&tmp); printf ("%i\n", *tmp); tscpaq_dumpq(&q, 7);
    tscpaq_dequeue (&q, (void**)&tmp); printf ("%i\n", *tmp); tscpaq_dumpq(&q, 8);
    tscpaq_dequeue (&q, (void**)&tmp); printf ("%i\n", *tmp); tscpaq_dumpq(&q, 9);
    /*tscpaq_dequeue (&q, (void**)&tmp);
